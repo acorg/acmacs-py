204c0f07e12df67bad23fd53e25141bbb4054469
Eugene Skepner <github@skepner.eu>
2021-03-16 17:13:44 +0100

    drawing map

--- cc/py-mapi.cc
+++ cc/py-mapi.cc
@@ -1,4 +1,5 @@
 #include "acmacs-base/quicklook.hh"
+#include "acmacs-base/range-v3.hh"
 #include "acmacs-chart-2/chart-modify.hh"
 #include "acmacs-map-draw/draw.hh"
 // #include "acmacs-chart-2/common.hh"
@@ -10,13 +11,12 @@
 void acmacs_py::mapi(py::module_& mdl)
 {
     using namespace pybind11::literals;
-    using namespace acmacs::chart;
 
-    py::class_<ChartDraw>(mdl, "ChartDraw")                                                                               //
-        .def(py::init<ChartModifyP, size_t>(), "chart"_a, "projection_no"_a = 0)                                                //
-        .def("calculate_viewport", &ChartDraw::calculate_viewport)                                                        //
-        .def("viewport", &ChartDraw::viewport, "by"_a = "acmacs_py")                                                      //
-        .def("transformation", [](const ChartDraw& chart_draw) { return chart_draw.chart(0).modified_transformation(); }) //
+    py::class_<ChartDraw>(mdl, "ChartDraw")                                                                                           //
+        .def(py::init<acmacs::chart::ChartModifyP, size_t>(), "chart"_a, "projection_no"_a = 0)                                       //
+        .def("calculate_viewport", &ChartDraw::calculate_viewport)                                                                    //
+        .def("viewport", &ChartDraw::viewport, "by"_a = "acmacs_py")                                                                  //
+        .def("transformation", [](const ChartDraw& chart_draw) { return chart_draw.chart(0).modified_transformation().as_vector(); }) //
         .def(
             "draw",
             [](const ChartDraw& chart_draw, const std::string& filename, double size, bool open) {
@@ -26,6 +26,22 @@ void acmacs_py::mapi(py::module_& mdl)
             },
             "filename"_a, "size"_a = 800.0, "open"_a = true) //
         ;
+
+    py::class_<acmacs::Viewport>(mdl, "Viewport")                                                     //
+        .def("__str__", [](const acmacs::Viewport& viewport) { return fmt::format("{}", viewport); }) //
+        .def("origin",
+             [](const acmacs::Viewport& viewport) {
+                 py::list res;
+                 for (acmacs::number_of_dimensions_t dim{0}; dim < viewport.origin.number_of_dimensions(); ++dim)
+                     res.append(viewport.origin[dim]);
+                 return res;
+             })
+        .def("size", [](const acmacs::Viewport& viewport) {
+            py::list res{2};
+            res[0] = viewport.size.width;
+            res[1] = viewport.size.height;
+            return res;
+        });
 
 } // acmacs_py::mapi
 
