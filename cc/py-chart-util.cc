f4ceccd993a86e169010ae968f22d73cbd2ba439
Eugene Skepner <github@skepner.eu>
2021-05-18 17:45:53 +0200

    ReferencePanelPlotData

--- cc/py-chart-util.cc
+++ cc/py-chart-util.cc
@@ -21,7 +21,7 @@ void acmacs_py::chart_util(py::module_& mdl)
         .def("make_antigen_serum_table", &ReferencePanelPlotData::make_antigen_serum_table, "antigens"_a, "sera"_a) //
         ;
 
-    py::class_<ReferencePanelPlotData::ASTable>(mdl, "ReferencePanelPlotDataASTable");
+    py::class_<ReferencePanelPlotData::AntigenSerumData>(mdl, "ReferencePanelPlotData_AntigenSerumData");
 
     py::class_<ReferencePanelPlot>(mdl, "ReferencePanelPlot") //
         .def(py::init<>())  //
