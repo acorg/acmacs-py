0b3f8527f79a510fcee9c7f03461b6117fe8dc68
Eugene Skepner <github@skepner.eu>
2021-05-21 10:00:30 +0200

    chain202105

--- py/acmacs_py/chain202105/run.py
+++ py/acmacs_py/chain202105/run.py
@@ -1,4 +1,4 @@
-import os
+import os, pprint
 from pathlib import Path
 from acmacs_py.error import KnownError
 
@@ -7,13 +7,20 @@ from acmacs_py.error import KnownError
 def run(chain_dir :Path):
     # with email.send_after():
     os.chdir(chain_dir)
-    load_setup()
+    chain_setup = load_setup()
 
 def load_setup():
+    locls = {}
+    setup_path = Path("Setup.py").resolve()
     try:
-        exec(Path("Setup.py").open().read())
+        exec(setup_path.open().read(), globals(), locls)
     except FileNotFoundError:
-        raise KnownError(f"invalid chain dir (no Setup.py): {os.getcwd()}")
+        raise KnownError(f"invalid chain dir (no {setup_path}): {os.getcwd()}")
+    try:
+        chain_setup_cls = locls["ChainSetup"]
+    except KeyError:
+        raise KnownError(f"invalid chain setup ({setup_path.resolve()}): ChainSetup class no defined")
+    return chain_setup_cls()
 
 # ----------------------------------------------------------------------
 
