dbbeb890b706bb15b5b41a024670582213ef4b9d
Eugene Skepner <github@skepner.eu>
2021-06-13 15:45:16 +0200

    avidity test

--- cc/py-avidity.cc
+++ cc/py-avidity.cc
@@ -41,6 +41,7 @@ void acmacs_py::avidity(py::module_& mdl)
         .def(
             "__iter__", [](const avidity::Results& results) { return py::make_iterator(results.results.begin(), results.results.end()); }, py::keep_alive<0, 1>()) //
         .def("__bool__", [](const avidity::Results& results) { return !results.results.empty(); })                                                                 //
+        .def("get", &avidity::Results::get, "antigen_no"_a)                                                                                                        //
         ;
 
     mdl.def(
