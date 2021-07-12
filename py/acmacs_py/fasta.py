4b648ca7a764960c5e09ff69c918a56538d5516c
Eugene Skepner <github@skepner.eu>
2021-07-12 13:08:13 +0200

    fasta

---
+++ py/acmacs_py/fasta.py
@@ -0,0 +1,34 @@
+# -*- Python -*-
+# license
+# license.
+# ======================================================================
+
+from pathlib import Path
+
+# ----------------------------------------------------------------------
+
+def read(filename :Path):
+    """read fasta file and return dict {name: sequence}"""
+    data = {}
+    name = None
+    sequence = ""
+    for line in filename.open("r"):
+        line = line.strip()
+        if not line:
+            pass
+        elif line[0] == ">":
+            if name:
+                data[name] = sequence
+            name = line[1:]
+            sequence = ""
+            # print(name)
+        else:
+            sequence += line
+    if name:
+        data[name] = sequence
+    return data
+
+# ======================================================================
+### Local Variables:
+### eval: (if (fboundp 'eu-rename-buffer) (eu-rename-buffer))
+### End:
