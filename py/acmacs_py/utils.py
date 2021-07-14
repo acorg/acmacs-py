580d2dc02c0143d4b00ea3c05b300dbeeefc771d
Eugene Skepner <github@skepner.eu>
2021-07-14 17:46:02 +0200

    py utiles.older_than

--- py/acmacs_py/utils.py
+++ py/acmacs_py/utils.py
@@ -2,13 +2,13 @@ from acmacs_py import *
 
 # ----------------------------------------------------------------------
 
-def older_than(first :Path, second :Path):
-    "returns if first file is older than second or first does not exist. raises if second does not exist"
+def older_than(first :Path, *other):
+    "returns if first file is older than second (and third, if passed) or first does not exist. raises if second does not exist"
     try:
         first_mtime = first.stat().st_mtime
     except FileNotFoundError:
         return True
-    return second.stat().st_mtime > first_mtime
+    return all(second.stat().st_mtime > first_mtime for second in other if second)
 
 # ======================================================================
 ### Local Variables:
